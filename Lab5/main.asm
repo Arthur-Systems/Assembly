6d2875b0bcbc028a7db344c855077d199d245955
Arthur-Systems <arthur.wei50@gmail.com>
2022-06-26 21:05:26 -0700

    Lab 5 Update

--- Lab5/main.asm
+++ Lab5/main.asm
@@ -1,29 +1,45 @@
-#program created by Arthur Wei 
 
-data: 
+ # @file main.asm
+ # @author Arthur Wei  
+ # @version Lab 5
+ # @date 2022-06-26
+
+ # @brief This file is the implementation of the Stack class.
+
+# ! Registers
+
+# eax 
+# ebx
+# ecx
+
+data:     # Define Variables
     x:
         .long 1 
         .long 2
         .long 3
         .long 4
         .long 0
+        .long 1 
+        .long 2
+        .long 3
+        .long 4
+
         
     sum:
         .long 0
 
-text: 
-    .globl _main
-    _main:
+text:  # Define Functions
+    .globl _main # Declare _main as a global function
+    _main: 
 
-    movl $5, %eax   #counter
-    movl $0, %ebx   #addition registery 
-    movl $x, %ecx   #Pointer 
+    movl $0, %ebx   # addition registery 
+    movl $x, %ecx   # Pointer 
 
 top:
-    addl (%ecx), %ebx 
-    addl $4, %eax
-    decl %eax
-    jnz top
+    addl (%ecx), %ebx  # add the value at the pointer to the addition registery
+    addl $4, %ecx  # add to the pointer to the counter
+    cmpl $0, (%ecx)
+    jne top
 
 done: movl %ebx, sum
     
