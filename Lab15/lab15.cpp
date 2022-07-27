b474726f29215dfe9c0401ae103081eb8daa7035
Arthur-Systems <arthur.wei50@gmail.com>
2022-07-27 16:53:03 -0700

    Lab 15 Update

---
+++ Lab15/lab15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int userInput();
+int main(){
+
+cout<<"Input(int): ";
+
+
+
+
+}
+
+int userInput(){
+string input;
+cin>>input;
+
+}
+
